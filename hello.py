08dc1dafaf1ca72b57d07bd55d8cd52e4a9b2ca9
FF7FSystem <yellowbear@yandex.ru>
2019-02-01 22:31:09 +0700

    edit app

--- hello.py
+++ hello.py
@@ -1,9 +1,8 @@
 def wsgi_app(envir,start_response):
-	print('Запустился wsgi_app')
-	status = '200 Ok'
-	headers = [('Content-Type', 'text/plant')]
-	body = [ i for i in envir['wsgi.input']]
-	#envir['QUERY_STRING']
-	print('В боди нахдится --- ',body)
-	start_response(status,headers)
-	return body
+    print('Запустился wsgi_app')
+    status = '200 Ok'
+    headers = [('Content-Type', 'text/plain')]
+    body = [bytes(i + '\n', 'ascii') for i in envir['QUERY_STRING'].split('$')
+    print('В боди нахдится --- ',body)
+    start_response(status,headers)
+    return body
