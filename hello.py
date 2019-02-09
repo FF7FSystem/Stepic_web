190a342523ee9d54d09962a460c8b03e7256545a
FF7FSystem <yellowbear@yandex.ru>
2019-02-09 22:42:04 +0700

    edit gunicorn.conf, app, init.sh

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ def wsgi_app(envir,start_response):
     print('Запустился wsgi_app')
     status = '200 Ok'
     headers = [('Content-Type', 'text/plain')]
-    body = [bytes(i + '\n', 'ascii') for i in envir['QUERY_STRING'].split('$')
+    body = [bytes(i + '\n', 'ascii') for i in envir['QUERY_STRING'].split('&')]
     print('В боди нахдится --- ',body)
     start_response(status,headers)
     return body
